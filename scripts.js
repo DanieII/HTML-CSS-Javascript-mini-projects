adaa85d9d2635aa68de91ecdd26f3fd0eee5987f
DanieII <danielbogdanov1748@gmail.com>
2023-04-25 23:41:49 +0300

    Added a section for the trending movies and fixed the header for smaller screen sizes

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// const button = document.getElementById("btn");
+// const searchField = document.getElementsByTagName("input")[0];
+// const storage = document.getElementById("result-container")
+
+// function getSeachData() {
+//     return searchField.value;
+// }
+
+// button.addEventListener("click", function() {
+//     let result = getSeachData();
+//     let currentDiv = document.createElement("div");
+//     let node = document.createTextNode(result);
+//     if (!node.textContent) {
+//         children = storage.children
+//         console.log(children)
+//         for (let i=0; i<children.length; i++) {
+//             current_element = children[i]
+//             console.log(`current element is ${current_element.textContent}`)
+//         }
+//         currentDiv.append("No items");
+//     }
+//     currentDiv.append(node)
+//     storage.appendChild(currentDiv);
+    
+// });
